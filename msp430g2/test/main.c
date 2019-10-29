c6a510df6bc594c360f7a31c31c9972a35df4ebf
Andrew Parlane <andrew.parlane@gmail.com>
2019-10-29 19:52:20 -0300

    Adding basic test project for working with the MSP430g2 launchpad.

---
+++ msp430g2/test/main.c
@@ -0,0 +1,60 @@
+/*
+ * ======== Standard MSP430 includes ========
+ */
+#include <msp430.h>
+#include <stdint.h>
+#include <string.h>
+
+/*
+ * ======== Grace related declaration ========
+ */
+extern void Grace_init(void);
+extern volatile uint16_t msSinceBoot;
+
+void uart_putc(char c)
+{
+    // Wait until the Tx buffer is empty
+    while (!(IFG2 & UCA0TXIFG));
+    UCA0TXBUF = c;
+}
+
+void uart_puts(const char *s)
+{
+    for (; *s != (int)NULL; s++)
+    {
+        uart_putc(*s);
+    }
+}
+
+/*
+ *  ======== main ========
+ */
+int main( void )
+{
+    Grace_init();                     // Activate Grace-generated configuration
+
+    // >>>>> Fill-in user code here <<<<<
+
+    uart_puts("Hello World!\n");
+
+    // enable global interrupts
+    __bis_SR_register(GIE);
+
+    // enable WDT irq
+    IE1 |= WDTIE;
+
+    uint16_t lastHB = 0;
+
+    while(1)
+    {
+        if ((msSinceBoot - lastHB) > 500)
+        {
+            lastHB = msSinceBoot;
+            P1OUT ^= BIT0;  // toggle LED
+            uart_puts("Toggle\n");
+        }
+    }
+
+
+    return 0;
+}
