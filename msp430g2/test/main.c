15cd670bfb8567c865cd91d7ddc15c0970ec413d
Andrew Parlane <andrew.parlane@gmail.com>
2019-10-30 11:27:13 -0300

    MSG430: Test: Added support for GPIOs (outputs) on the DLP-7970ABP boosterpack

--- msp430g2/test/main.c
+++ msp430g2/test/main.c
@@ -4,6 +4,7 @@
 #include <msp430.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
 
 /*
  * ======== Grace related declaration ========
@@ -26,6 +27,71 @@ void uart_puts(const char *s)
     }
 }
 
+void set_trf7970a_ss(bool en)   // slave select (en = 1 -> ss = 0 = chip active)
+{
+    // P2.1
+    if (en)
+    {
+        P2OUT &= ~BIT1; // ss = 0 (active low)
+    }
+    else
+    {
+        P2OUT |= BIT1;  // ss = 1 (active low)
+    }
+}
+
+void set_trf7970a_enable(bool en)
+{
+    // P2.2
+    if (en)
+    {
+        P2OUT |= BIT2;  // enable = 1
+    }
+    else
+    {
+        P2OUT &= ~BIT2; // enable = 0
+    }
+}
+
+void set_dlp_led1(bool on)  // labelled as ISO14443B
+{
+    // P2.3
+    if (on)
+    {
+        P2OUT |= BIT3;
+    }
+    else
+    {
+        P2OUT &= ~BIT3;
+    }
+}
+
+void set_dlp_led2(bool on)  // labelled as ISO14443A
+{
+    // P2.4
+    if (on)
+    {
+        P2OUT |= BIT4;
+    }
+    else
+    {
+        P2OUT &= ~BIT4;
+    }
+}
+
+void set_dlp_led3(bool on)  // labelled as ISO15693
+{
+    // P2.5
+    if (on)
+    {
+        P2OUT |= BIT5;
+    }
+    else
+    {
+        P2OUT &= ~BIT5;
+    }
+}
+
 /*
  *  ======== main ========
  */
@@ -39,6 +105,13 @@ int main( void )
     //       For ease of diffing, to see what various changes in the GRACE GUI do to the code
     //       For real projects the code should be tidied up some more.
 
+    // Set up the DLP-7970ABP boosterpack
+    set_trf7970a_ss(false);
+    set_trf7970a_enable(false);
+    set_dlp_led1(true);
+    set_dlp_led2(false);
+    set_dlp_led3(false);
+
     // UART Tx test
     uart_puts("Hello World!\n");
 
@@ -49,13 +122,26 @@ int main( void )
 
     // 16 bit CPU, so 32 bit ops are only to be used when necessary
     uint32_t lastHB = 0;
+    uint8_t dlp_leds = 0x01;
     while(1)
     {
         if ((msSinceBoot - lastHB) > 500)
         {
             lastHB = msSinceBoot;
-            P1OUT ^= BIT0;  // toggle LED
-            uart_puts("Toggle\n");
+            uart_puts("Timeout\n");
+
+            // toggle LED
+            P1OUT ^= BIT0;
+
+            // update DLP LEDs
+            dlp_leds <<= 1;
+            if (dlp_leds > 0x04)
+            {
+                dlp_leds = 0x01;
+            }
+            set_dlp_led1(dlp_leds & 0x01);
+            set_dlp_led2(dlp_leds & 0x02);
+            set_dlp_led3(dlp_leds & 0x04);
         }
     }
 
