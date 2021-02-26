88e3243b82a861fa81628a5b98666f7ac26130ae
Andrew Parlane <andrew.parlane@gmail.com>
2021-02-26 11:09:20 -0300

    LibPrep: Adding more comments to the lib_prep script.
    Including the standard header.

--- hdl/synth_pnr/lib_prep/lib_prep.tcl
+++ hdl/synth_pnr/lib_prep/lib_prep.tcl
@@ -1,6 +1,40 @@
-source ../common/libraries.tcl
+# ----------------------------------------------------------------------------------
+#        File: lib_prep.tcl
+# Description: TCL script to prepare NDM libraries for use with ICC2
+#      Author: Andrew Parlane
+# ----------------------------------------------------------------------------------
+
+# This file is part of https://github.com/andrewparlane/fiuba_thesis
+# Copyright (c) 2020 Andrew Parlane.
+#
+# This is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this code. If not, see <http://www.gnu.org/licenses/>.
+
+# =============================================================================
+# scripts
+# =============================================================================
+
+# colourisation
 source ../common/colourisation.tcl
+
+# debug
 source ../common/debug.tcl
+
+# libs
+source ../common/libraries.tcl
+
+# =============================================================================
+# Helper functions
+# =============================================================================
 
 proc convert_lib { libname techfile lefs db_dir} {
     variable COLOUR_BLUE
@@ -44,6 +78,10 @@ proc convert_lib { libname techfile lefs db_dir} {
     return 1
 }
 
+# =============================================================================
+# Settings
+# =============================================================================
+
 set_app_options -name lib.workspace.save_design_views -value true
 set_app_options -name lib.workspace.save_layout_views -value true
 set_app_options -name lib.workspace.keep_all_physical_cells -value true
@@ -54,6 +92,10 @@ set_app_options -name lib.logic_model.auto_remove_timing_only_designs -value tru
 set voltages        {1.62 1.8 1.98}
 set temperatures    {-40 0 25 85 125 150 175}
 set_pvt_configuration -clear_filter all -add -name all -process_labels {""} -process_numbers {1} -voltages $voltages -temperatures $temperatures
+
+# =============================================================================
+# Tech only
+# =============================================================================
 
 # Create our technology only NDM lib, see:
 # https://solvnet.synopsys.com/dow_retrieve/latest/kb/dg/icc2/videos/Library_Preparation_NDM_Demystified.mp4
@@ -92,6 +134,10 @@ if {($pause_between_commands == 1) && ([do_continue] == 0)} {
     return
 }
 
+# =============================================================================
+# D_CELLS_HD
+# =============================================================================
+
 # Create the D_CELLS_HD lib
 puts "[colour $COLOUR_BLUE]Creating D_CELLS_HD CLIB[clear_colour]"
 set res         [convert_lib "d_cells_hd" $NDM_TECHFILE $PDK_D_CELLS_HD_LEFS $PDK_D_CELLS_HD_LIBERTY_DIR]
@@ -102,6 +148,10 @@ if {$res == 0} {
 if {($pause_between_commands == 1) && ([do_continue] == 0)} {
     return
 }
+
+# =============================================================================
+# D_CELLS_HDLL
+# =============================================================================
 
 # Create the D_CELLS_HDLL lib
 puts "[colour $COLOUR_BLUE]Creating D_CELLS_HDLL CLIB[clear_colour]"
